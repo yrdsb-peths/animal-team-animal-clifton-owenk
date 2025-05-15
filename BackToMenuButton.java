1dc9b291c6432c9c614f77323040071f7a2da0bc
OwenKung0124 <440002825@gapps.yrdsb.ca>
2025-05-15 17:12:22 -0400

    Add back to menu button animation

--- BackToMenuButton.java
+++ BackToMenuButton.java
@@ -2,18 +2,44 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class BackToMenuButton extends Actor
 {
- 
+    private GreenfootImage img1 = new GreenfootImage("back_button.png");
+    private GreenfootImage img2 = new GreenfootImage("back_button2.png");
+    private boolean showingFirst = true;
+    private SimpleTimer animationTimer = new SimpleTimer();
+    
     public BackToMenuButton()
     {
-        GreenfootImage img = new GreenfootImage("back_button.png");
-        img.scale(200, 75);
-        setImage(img);
+        img1.scale(200, 75);
+        img2.scale(200, 75);
+        setImage(img1);
+        animationTimer.mark();
     }
+    
     public void act()
     {
+        animateButton();
+        
         if(Greenfoot.mouseClicked(this))
         {
             Greenfoot.setWorld(new TitleScreen());
+        }
+    }
+    
+    private void animateButton()
+    {
+        if(animationTimer.millisElapsed()>500)
+        {
+            if(showingFirst)
+            {
+                setImage(img2);
+            }
+            else
+            {
+                setImage(img1);
+            }
+            
+            showingFirst =!showingFirst;
+            animationTimer.mark();
         }
     }
 }
