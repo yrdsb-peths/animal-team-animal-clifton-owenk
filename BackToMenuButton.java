832328d47b459108e6fbdaca8a28a92fc6903bb3
OwenKung0124 <440002825@gapps.yrdsb.ca>
2025-05-15 17:26:05 -0400

    Click sound added to button

--- BackToMenuButton.java
+++ BackToMenuButton.java
@@ -1,19 +1,37 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
 public class BackToMenuButton extends Actor
 {
- 
+    private GreenfootImage img1 = new GreenfootImage("back_button.png");
+    private GreenfootImage img2 = new GreenfootImage("back_button2.png");
+    private boolean showingFirst = true;
+    private SimpleTimer animationTimer = new SimpleTimer();
+    private GreenfootSound clickSound = new GreenfootSound("button_click.mp3");
+
     public BackToMenuButton()
     {
-        GreenfootImage img = new GreenfootImage("back_button.png");
-        img.scale(200, 75);
-        setImage(img);
+        img1.scale(200, 75);
+        img2.scale(200, 75);
+        setImage(img1);
+        animationTimer.mark();
     }
+
     public void act()
     {
-        if(Greenfoot.mouseClicked(this))
-        {
+        animateButton();
+
+        if (Greenfoot.mouseClicked(this)) {
+            clickSound.play();  //Play sound when click
             Greenfoot.setWorld(new TitleScreen());
+        }
+    }
+
+    private void animateButton()
+    {
+        if (animationTimer.millisElapsed() > 500) {
+            setImage(showingFirst ? img2 : img1);
+            showingFirst = !showingFirst;
+            animationTimer.mark();
         }
     }
 }
