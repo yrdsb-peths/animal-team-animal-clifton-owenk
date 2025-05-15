753e7909fce8e53fa42bc31999ccecdc47358ea1
OwenKung0124 <440002825@gapps.yrdsb.ca>
2025-05-15 18:51:21 -0400

    Add title

---
+++ TitleImage.java
@@ -0,0 +1,11 @@
+import greenfoot.*;
+
+public class TitleImage extends Actor
+{
+    public TitleImage()
+    {
+        GreenfootImage img = new GreenfootImage("title_chinese.png");
+        img.scale(250, 250);
+        setImage(img);
+    }
+}
