bc3ef0ad44e69afdade387fe47d742d4608ca941
OwenKung0124 <440002825@gapps.yrdsb.ca>
2025-05-15 23:39:16 -0400

    update title to english

--- TitleImage.java
+++ TitleImage.java
@@ -5,7 +5,7 @@ public class TitleImage extends Actor
     public TitleImage()
     {
         GreenfootImage img = new GreenfootImage("title_chinese.png");
-        img.scale(250, 250);
+        img.scale(400, 160);
         setImage(img);
     }
 }
